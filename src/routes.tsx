84f5a6f539e4ee99225bc5bcc454941d13b25e07
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-17 19:38:06 -0300

    Update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import React, { ReactElement } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Main from './pages/Main'
+import Dashboard from './pages/Dashboard'
+import Pacients from './pages/Pacients'
 
 function Routes(): ReactElement {
   return (
     <BrowserRouter>
-      <Route path="/" component={Main} exact />
+      <Route path="/" component={Dashboard} exact />
+      <Route path="/pacients" component={Pacients} />
     </BrowserRouter>
   )
 }
