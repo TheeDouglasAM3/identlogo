1d919a55faa973249518f7e6ddbfb4f587d151f8
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-17 03:13:38 -0300

    Update JSX and styles from Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,11 +4,16 @@ import { Container } from '../styles/pages/main'
 import Navbar from '../components/Navbar'
 import UserMenu from '../components/UserMenu'
 import CardGeneralInformation from '../components/CardGeneralInformation'
+import ButtonDefault from '../components/ButtonDefault'
+import CardAttendance from '../components/CardAttendance'
+import CardMostPerformedTest from '../components/CardMostPerformedTest'
 
 import userIcon from '../images/userIcon.png'
 import testTubeIcon from '../images/testTube.png'
 import testTubeCorrectIcon from '../images/testTubeCorrect.png'
 import testTubeWrongIcon from '../images/testTubeWrong.png'
+import uilBrain from '../images/uilBrain.png'
+import uilCell from '../images/uilCell.png'
 
 const Main = (): ReactElement => (
   <Container>
@@ -57,12 +62,58 @@ const Main = (): ReactElement => (
           <div className="historico-atendimento">
             <div className="title-and-button">
               <h3>Histórico de atendimentos</h3>
-              <span className="button-default">Ver todos</span>
+              <ButtonDefault text="VER TODOS" />
+            </div>
+            <div className="content-tests">
+
+              <CardAttendance
+                namePerson="Renata Augusto Ferreira"
+                nameTest="TDAH CAB ADHD"
+                date="20/10/2023"
+              >
+                <img src={uilBrain} alt="icon" />
+              </CardAttendance>
+
+              <CardAttendance
+                namePerson="Renata Augusto Ferreira"
+                nameTest="Autismo"
+                date="20/09/2023"
+              >
+                <img src={uilCell} alt="icon" />
+              </CardAttendance>
+
+              <CardAttendance
+                namePerson="Renata Augusto Ferreira"
+                nameTest="TDAH CAB ADHD"
+                date="20/10/2023"
+              >
+                <img src={uilBrain} alt="icon" />
+              </CardAttendance>
+
+              <CardAttendance
+                namePerson="Renata Augusto Ferreira"
+                nameTest="Autismo"
+                date="20/09/2023"
+              >
+                <img src={uilCell} alt="icon" />
+              </CardAttendance>
+
             </div>
           </div>
           <div className="teste-mais-realizado">
             <h3>Teste mais realizado</h3>
+
+            <CardMostPerformedTest
+              nameTest="Autismo"
+              numberTests={13}
+            >
+              <img src={uilCell} alt="icon" />
+            </CardMostPerformedTest>
+
           </div>
+        </div>
+        <div className="atualizacoes-importantes">
+          <h3>Atualizações Importantes</h3>
         </div>
       </div>
     </div>
--- src/styles/pages/main.ts
+++ src/styles/pages/main.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
 import imgBackground from '../../images/background.png'
+import imgCardBackground from '../../images/retangular.png'
 
 export const Container = styled.div`
   #page-main {
     width: 100vw;
     height: 100vh;
-    background-image: url('${imgBackground}'), linear-gradient(90deg, #0094FF, #00d689 90%);
+    background-image: url('${imgBackground}'), linear-gradient(90deg, #0859d1, #00d689 90%);
     display: flex;
     justify-content: space-between;
   }
@@ -19,7 +20,8 @@ export const Container = styled.div`
   }
 
   .informacoes-gerais,
-  .historico-atendimento {
+  .historico-atendimento,
+  .atualizacoes-importantes {
     width: 100%;    
     padding: 0.6rem 0rem 0rem 2rem;
   }
@@ -30,6 +32,13 @@ export const Container = styled.div`
     width: 100%;
     height: 10rem;
   }
+
+  .content-tests {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
   .work-session {
     display: flex;
     align-items: center;
@@ -38,24 +47,32 @@ export const Container = styled.div`
 
   .work-session .historico-atendimento {
     width: 76%;
+    margin-right: 1.4rem;
   }
 
   .work-session .historico-atendimento .title-and-button {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
   }
 
   .work-session .historico-atendimento .title-and-button h3 {
     margin-right: 0.8rem;
   }
 
-  .button-default {
-    background: linear-gradient(93.05deg, #00A3FF 0%, #0057FF 100%);
-    border-radius: 20px;
-    color: #FFFFFF;
-    padding: 0.2rem 1.2rem 0.2rem 1.2rem;
-    cursor: pointer;
-    font-size: 16px;
+  .work-session .teste-mais-realizado {
+    width: 24%;
+    margin-right: 1.4rem;
+  }
+
+  .work-session .teste-mais-realizado h3 {
+    margin-top: 0.6rem;
+  }
+
+  .content-tests {
+    width: 100%;
+    height: 15rem;
+    margin-top: 0.6rem;
+    background: #FFFFFF;
   }
 
 `
